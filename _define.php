e0c626d0eb03931b1b409d7ddb7231b4a258a611
franck <carnet.franck.paul@gmail.com>
2025-04-03 09:28:10 +0200

    New release 6.0

--- _define.php
+++ _define.php
@@ -15,9 +15,9 @@ $this->registerModule(
     'Typo',
     'Brings smart typographic replacements for your blog entries and comments',
     'Franck Paul and contributors',
-    '5.2.1',
+    '6.0',
     [
-        'date'        => '2025-01-21T18:43:58+0100',
+        'date'        => '2025-04-03T09:28:10+0200',
         'requires'    => [['core', '2.28']],
         'permissions' => 'My',
         'type'        => 'plugin',
