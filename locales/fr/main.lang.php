266a1e5843c49dd07612aeabdf56401794b4a15d
franck <carnet.franck.paul@gmail.com>
2025-12-15 15:22:05 +0100

    French locales

--- locales/fr/main.lang.php
+++ locales/fr/main.lang.php
@@ -10,9 +10,10 @@
 
 use Dotclear\Helper\L10n;
 
-L10n::$locales['Typo'] = 'Typo';
+L10n::$locales['Typography'] = 'Typographie';
 L10n::$locales['Brings smart typographic replacements for your blog entries and comments'] = 'Applique des remplacements typographiques pour les billets et les commentaires de votre blog';
 L10n::$locales['Typographic replacements'] = 'Remplacements typographiques';
+L10n::$locales['Typo'] = 'Typo';
 L10n::$locales['Pages'] = 'Pages';
 L10n::$locales['Entries'] = 'Entrées';
 L10n::$locales['Warning! These replacements will not be undoable.'] = 'Attention ces remplacements sont irréversibles.';
@@ -22,8 +23,12 @@ L10n::$locales['"---" for em-dashes; "--" for en-dashes'] = '"---" (3) pour les 
 L10n::$locales['"--" for em-dashes; "---" for en-dashes'] = '"--" (2) pour les tirets longs (em-dashes) ; "---" (3) pour les tirets courts (en-dash)';
 L10n::$locales['Enable typographic replacements for this blog'] = 'Activer les remplacements typographiques pour ce blog';
 L10n::$locales['Options'] = 'Options';
-L10n::$locales['Enable typographic replacements for entries'] = 'Activer les remplacements typographiques pour les billets';
+L10n::$locales['Enable typographic replacements for entry titles'] = 'Activer les remplacements typographiques pour le titre des entrées';
+L10n::$locales['Enable typographic replacements for entry contents'] = 'Activer les remplacements typographiques pour le contenu des entrées';
 L10n::$locales['Enable typographic replacements for comments'] = 'Activer les remplacements typographiques pour les commentaires';
 L10n::$locales['Excluding trackbacks'] = 'Rétroliens exclus';
-L10n::$locales['Enable typographic replacements for categories'] = 'Activer les remplacements typographiques pour les catégories';
+L10n::$locales['Enable typographic replacements for category titles'] = 'Activer les remplacements typographiques pour le titre des catégories';
+L10n::$locales['Enable typographic replacements for category descriptions'] = 'Activer les remplacements typographiques pour la description des catégories';
+L10n::$locales['Enable typographic replacements for simpleMenu labels and descriptions'] = 'Activer les remplacements typographiques pour les labels et les descriptions des entrées du menu simple';
+L10n::$locales['Enable typographic replacements for blogroll titles, descriptions and categories'] = 'Activer les remplacements typographiques pour les titres, les descriptions et les catégories de la liste des liens';
 L10n::$locales['Dashes replacement mode'] = 'Mode de remplacement des tirets multiples';
